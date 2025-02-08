2888132b3ac700759643221e39317b0bb85aaf43
Evgeniy Saenko <essaenko@ro.ru>
2025-02-08 15:05:21 +0300

    New case scenario with nested collections in schema

--- src/server/rooms/schema/MyRoomState.ts
+++ src/server/rooms/schema/MyRoomState.ts
@@ -6,9 +6,20 @@ export class Component extends Schema {
   @type("number") value: number;
 }
 
+export class ComponentWithArray extends Component {
+  @type(["string"]) spells: ArraySchema<string> = new ArraySchema();
+}
+
 export class Entity extends Schema {
   @type("string") id: string = nanoid(9);
   @type([Component]) components = new ArraySchema<Component>();
+
+  constructor() {
+    super();
+
+    this.components.push(new Component());
+    this.components.push(new ComponentWithArray());
+  }
 }
 
 export class MyRoomState extends Schema {
